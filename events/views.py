7ea4573574d1e867c663276bd5cd776f8dcd56e1
Ricardo Santos <ricardo.ams@hotmail.com>
2020-08-02 21:36:24 +0000

    added sorting and counts

--- events/views.py
+++ events/views.py
@@ -3,7 +3,6 @@ from django.contrib import messages
 from django.db.models import Q
 from .models import Event, Category
 
-
 # Create your views here.
 
 def all_events(request):
@@ -12,8 +11,24 @@ def all_events(request):
     events = Event.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                events = events.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            events = events.order_by(sortkey)
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             events = events.filter(category__name__in=categories)
@@ -28,12 +43,17 @@ def all_events(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             events = events.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'events': events,
         'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'events/events.html', context)
+
 
 def event_detail(request, event_id):
     """ A view to show individual event details """
