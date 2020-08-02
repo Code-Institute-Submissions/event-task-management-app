a2764e30ee969052b449f5d4976d7566b41de0b5
Ricardo Santos <ricardo.ams@hotmail.com>
2020-08-02 16:23:10 +0000

    customized admin

--- events/admin.py
+++ events/admin.py
@@ -2,5 +2,24 @@ from django.contrib import admin
 from .models import Event, Category
 
 # Register your models here.
-admin.site.register(Event)
-admin.site.register(Category)
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Category, CategoryAdmin)
