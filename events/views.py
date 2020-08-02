f952a75cf6e1442fbab057c7cc6ea80ce3a25e3d
Ricardo Santos <ricardo.ams@hotmail.com>
2020-08-02 20:22:59 +0000

    added filtering

--- events/views.py
+++ events/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Event
+from .models import Event, Category
 
 
 # Create your views here.
@@ -11,8 +11,14 @@ def all_events(request):
 
     events = Event.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            events = events.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
