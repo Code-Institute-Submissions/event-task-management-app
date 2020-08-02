1f5a59f6df56e700c025dfdeb53603abe716c4e0
Ricardo Santos <ricardo.ams@hotmail.com>
2020-08-02 16:32:09 +0000

    added events views and templates

--- events/views.py
+++ events/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Event
 
 # Create your views here.
+
+def all_events(request):
+    """ A view to show all products, including sorting and search queries """
+
+    events = Event.objects.all()
+
+    context = {
+        'events': events,
+    }
+
+    return render(request, 'events/events.html', context)
