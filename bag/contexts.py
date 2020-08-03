4b7c48e6918082a1ebcd3fad65b828e9e00f0266
Ricardo Santos <ricardo.ams@hotmail.com>
2020-08-03 14:17:51 +0000

    added quantity

--- bag/contexts.py
+++ bag/contexts.py
@@ -10,15 +10,27 @@ def bag_contents(request):
     event_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        event = get_object_or_404(Event, pk=item_id)
-        total += quantity * event.price
-        event_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'event': event,
-        })
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            event = get_object_or_404(Event, pk=item_id)
+            total += item_data * event.price
+            event_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'event': event,
+            })
+        else:
+            event = get_object_or_404(Event, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * event.price
+                event_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'event': event,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
