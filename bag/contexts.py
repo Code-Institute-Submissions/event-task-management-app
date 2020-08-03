2664c34d0b8deb3af1ab35b8408e61fb718cb639
Ricardo Santos <ricardo.ams@hotmail.com>
2020-08-03 14:31:11 +0000

    quantity to checkout added

--- bag/contexts.py
+++ bag/contexts.py
@@ -27,7 +27,7 @@ def bag_contents(request):
                 event_count += quantity
                 bag_items.append({
                     'item_id': item_id,
-                    'quantity': item_data,
+                    'quantity': quantity,
                     'event': event,
                     'size': size,
                 })
