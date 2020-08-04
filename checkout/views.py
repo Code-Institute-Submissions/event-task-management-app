38e0d276ab7ae3f9cfd3d729fd8a56e43df16607
Ricardo Santos <ricardo.ams@hotmail.com>
2020-08-04 11:40:59 +0000

    added stripe and stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_UkZJgBU93HpFICbFDhOSZQvj006KGsTG2O',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
