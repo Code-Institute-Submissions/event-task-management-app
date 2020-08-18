959b9bd25d54be286e21747996a11e0a799d24ec
Ricardo Santos <ricardo.ams@hotmail.com>
2020-08-18 10:42:51 +0000

    secret key removed

--- EventTaskApp/settings.py
+++ EventTaskApp/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#!_-$g+5qu_ejqjfzga94y$sj-_5$vl3u%aut&$qg0(pe-(e2q'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['event-task-app.herokuapp.com', 'localhost']
 
