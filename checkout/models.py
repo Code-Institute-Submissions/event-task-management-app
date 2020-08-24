1eb7355495bd846f6d8d3a7ecb92f1ef9f489476
Ricardo Santos <ricardo.ams@hotmail.com>
2020-08-24 11:22:45 +0000

    models.py

--- checkout/models.py
+++ checkout/models.py
@@ -77,4 +77,4 @@ class OrderLineItem(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'SKU {self.event.sku} on order {self.order.order_number}'
+        return f'id {self.event.id} on order {self.order.order_number}'
